34af6228cee8c76dfbc10a7950dc2f9643aafb3e
Grand Marcell <grand1310marcell@gmail.com>
2024-01-13 11:44:23 +0700

    add vue to skills

--- constants/index.ts
+++ constants/index.ts
@@ -163,6 +163,12 @@ export const Skill_data = [
       width: 80,
       height: 80,
     },
+    {
+      skill_name: "VueJS",
+      Image: "/vue.png",
+      width: 80,
+      height: 80,
+    },
   ];
   
   export const Backend_skill = [
